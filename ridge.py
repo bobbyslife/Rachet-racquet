89b7bdd9a39272f6fcf21671f1580a9a562088cb
Laura Pak <laurapak2@gmail.com>
2017-11-01 20:24:58 -0500

    book code into ridge.py

--- ridge.py
+++ ridge.py
@@ -1,1 +1,33 @@
-#rigid
+#ride regression from Machine Learning in Action
+#actually just copying the Listing 8.3
+
+#calculates weights
+#implements ridge regression for any given value of lambda (default .2, var actually lam bc 'lambda' is a python keyword)
+#construct matrix xTx
+def ridgeRegres(xMat, yMat, lam=.2):
+	xTx = xMat.T*xMat
+	denom = xTx + eye(shape(xMat)[1])*lam
+	if linalg.det(denom) == 0.0:
+		print("This matrix is singular, cannot do inverse")
+		return
+	ws = denom.I * (xMat.T*yMat)
+	return ws
+
+#tests weights over a number of lambda values
+def ridgeTest(xArr, yArr):
+	xMat= mat(xArr); yMat = mat(yArr).T
+	yMean = mean(yMat,0)
+	yMat = yMat - yMean
+	xMeans = mean(xMat,0)
+	xVar = var(xMat,0)
+	xMat = (xMat - xMeans)/xVar
+	numTestPts = 30
+	wMat = zeros((numTestPts,shape(xMat)[1]))
+	for i in range(numTestPts):
+		ws = ridgeRegres(xMat,yMat,exp(i-10))
+		wMat[i,:]=ws.T
+	return wMat
+
+
+
+
